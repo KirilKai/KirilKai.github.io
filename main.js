3bcacac08f9681f1a04526f1d0b28ec251ad7e47
Kiril <kaikovkiril@gmail.com>
2023-08-26 02:14:45 +0300

    scroll action

--- main.js
+++ main.js
@@ -32,3 +32,22 @@ cards.forEach((card) => {
     }
   });
 });
+
+//Reveal on scroll
+window.addEventListener('scroll', reveal);
+
+function reveal() {
+  var reveals = document.querySelectorAll('.reveal');
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowheight = window.innerHeight;
+    var revealtop = reveals[i].getBoundingClientRect().top;
+    var revealpoint = 150;
+
+    if (revealtop < windowheight - revealpoint) {
+      reveals[i].classList.add('visible');
+    } else {
+      reveals[i].classList.remove('visible');
+    }
+  }
+}
